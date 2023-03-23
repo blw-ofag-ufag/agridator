a3762c53bc8e147700d9a3752b46ee0f0c011684
ebudem <e.demirhan99@hotmail.com>
2023-03-23 14:41:23 +0100

    Add data service

--- agridator-frontend/src/app/app.component.ts
+++ agridator-frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from './service/data.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'agridator-frontend';
+  title = 'Agridator';
+  actions : string[] = [];
+
+  constructor(private dataService: DataService) {
+    this.actions = this.dataService.getActions();
+  }
+
 }
---
+++ agridator-frontend/src/app/service/data.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor() { }
+
+  getActions ()
+  {
+    return [ 
+      "Aktion A",
+      "Aktion B",
+      "Aktion C",
+    ]
+  }
+}
