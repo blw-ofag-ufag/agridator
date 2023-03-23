f5515fda9ed4bb1472049dfbe750b08a12efd601
Beat Gubser <gubser@gmx.ch>
2023-03-23 15:39:04 +0100

    new components

---
+++ agridator-frontend/src/app/components/post-tracking-infos/post-tracking-infos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-tracking-infos',
+  templateUrl: './post-tracking-infos.component.html',
+  styleUrls: ['./post-tracking-infos.component.scss']
+})
+export class PostTrackingInfosComponent {
+
+}
