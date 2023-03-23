94992e99ee7e8fe7a6cb23a07f8ceb34b252daaf
Beat Gubser <gubser@gmx.ch>
2023-03-23 15:39:21 +0100

    new components

--- agridator-frontend/src/app/app.module.ts
+++ agridator-frontend/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PreTrackingInfosComponent } from './components/pre-tracking-infos/pre-tracking-infos.component';
+import { TrackingComponent } from './components/tracking/tracking.component';
+import { PostTrackingInfosComponent } from './components/post-tracking-infos/post-tracking-infos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PreTrackingInfosComponent,
+    TrackingComponent,
+    PostTrackingInfosComponent
   ],
   imports: [
     BrowserModule
