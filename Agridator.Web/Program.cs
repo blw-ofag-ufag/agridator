d18920bf620bd85271e3045adcf5bae31b5dfc9b
Patrick Nanzer <patrick.nanzer@rubicon.eu>
2023-03-23 16:57:53 +0100

    Add Swagger

--- Agridator.Web/Program.cs
+++ Agridator.Web/Program.cs
@@ -1,14 +1,26 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "agridator Backend", Version = "v1" });
+    c.EnableAnnotations();
+});
 
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseStaticFiles();
