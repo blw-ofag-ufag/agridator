7bb5c0bf521d3cb4c3fce4117bf02d9918a26203
Paulius <lazdalt@gmail.com>
2023-03-24 16:40:55 +0100

    drop al tables

--- Agridator.Web/Program.cs
+++ Agridator.Web/Program.cs
@@ -48,7 +48,13 @@ using (var scope = app.Services.CreateScope())
     {
         var context = services.GetService<ApplicationDbContext>();
 
-        context.Database.EnsureDeleted();
+        context.Database.ExecuteSqlRaw(@"DO $$ DECLARE r RECORD;
+BEGIN
+  FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = current_schema()) LOOP
+    EXECUTE 'DROP TABLE ' || quote_ident(r.tablename) || ' CASCADE';
+  END LOOP;
+END $$;");
+
         context.Database.Migrate();
         Task.Run(async () => await DataSeeder.SeedDataAsync(context));
 
